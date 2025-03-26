9cfa2f9c87dd18301d7ad8bd29bf1ba906d9357a
Sw720259 <swathi.v1@s.amity.edu>
2025-03-26 13:34:40 +0530

    print the btech in cse

---
+++ switch.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+	int course;
+	printf("enter a course:");
+	scanf("%d",&course);
+	switch(course) {
+		case 1:
+			printf("btech in CSE\n");
+			break;
+			return 0;
+	}

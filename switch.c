5c7c0661d71995200ab5454603a01cf29dd7006d
Sw720259 <swathi.v1@s.amity.edu>
2025-03-26 13:45:54 +0530

    print the cource cse

--- switch.c
+++ switch.c
@@ -7,15 +7,7 @@ int main() {
 		case 1:
 			printf("btech in CSE\n");
 			break;
-		case 2:
-			printf("btech in AIML\n");
-			break ;
-		case 3:
-			printf("btech in ECE\n");
-			break;
-		case 4:
-			printf("btech in mechanical\n");
-			break;
+		
 
 			return 0;
 }
